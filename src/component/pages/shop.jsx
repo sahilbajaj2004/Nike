557be15ec868f5c89395661937342af9d56c2d89
Sahil bajaj <sahilbajaj0941@gmail.com>
2025-05-28 15:00:02 +0530

    pakka last

--- src/component/pages/shop.jsx
+++ src/component/pages/shop.jsx
@@ -4,19 +4,19 @@ const products = [
   {
     id: 1,
     name: 'Nike Air Max',
-    price: '₹17869',
+    price: '₹3469',
     img: 'https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcSPNGiECenbSjl3gd0_YYQKp2Fx3wnplIafS0dP2qmb88lh9KY0UZ2x5Mi3wuJXCTCP9NMEy03DjMveVcdpg3JiP9pJaapGwTvg-7HclC92S2QlO1M3Ks2oMQ'
   },
   {
     id: 2,
     name: 'Nike Revolution',
-    price: '₹9090',
+    price: '₹900',
     img: 'https://encrypted-tbn3.gstatic.com/shopping?q=tbn:ANd9GcTIAvc28GxvS-74j9jJyN7HDYQVAK1a0CJdMNOIitKXeoQ6F2FHcwxKZEwIGBG-Xm6aaJLLXl6jkoWciWjOdjMMxVDX3TplJvZEWRvWTV-jmNiD7LDfpo-zBg'
   },
   {
     id: 3,
     name: 'Nike Pegasus',
-    price: '₹18980',
+    price: '₹980',
     img: 'https://encrypted-tbn2.gstatic.com/shopping?q=tbn:ANd9GcStL6Cslx_Y1slrRLppU_CCzBOAGeQXrOYDYTsFTRy_8sgI584KJfH8dhsrtZe2oJODlj-KZm4385apyivBd8t0nts4M6OVY0bJjmlsoJYBvCBPzpLrt4x9sA'
   },
   {
@@ -28,13 +28,13 @@ const products = [
   {
     id: 5,
     name: 'Nike ZoomX',
-    price: '₹9878',
+    price: '₹2878',
     img: 'https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcRxFYiOcWJe1codWTHZdjAvJt7GPiqbqoFZR71qlZlppZQ4j0VPAjmM5MhR8doK-Dt9Pf0Y5Ryzsv3K9AQfnif6P6HUQI9jLSTRx6NSUYhwdvQCI0HGcMPokQ'
   },
   {
     id: 6,
     name: 'Nike Blazer Mid',
-    price: '₹10001',
+    price: '₹1981',
     img: 'https://encrypted-tbn2.gstatic.com/shopping?q=tbn:ANd9GcTswXRa3b5AbWo9gPgl3rRbQYqYffH8JJOHz_9ItnR_Y0Se2ytBw2YiaWjEl531al270cWtM3oVDIDIOEALBAJZejf1eIx5tTako0KTshxcYZQr-ZAEYgT39A'
   },
   {
@@ -46,7 +46,7 @@ const products = [
   {
     id: 8,
     name: 'Nike React Infinity',
-    price: '₹7867',
+    price: '₹5867',
     img: 'https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcRF0bBH8fbMvXRmEDlaN3xiWwjW49ecOECxuK2xmKIbqJHm-9J9RfpISTyWGn26q2TWbS8jq8GpkQbkNmq6LD9Q3fw5oYbhXuQi7cvND4i00Z6IHLfW49Efx18'
   }
 ];
